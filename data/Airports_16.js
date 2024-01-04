acb8401a9c8010d986580cd0b31b776d05f093ea
Manoj Pariyar <114010808+manojpariyar@users.noreply.github.com>
2024-01-04 21:12:03 +0100

    files uploaded

---
+++ data/Airports_16.js
@@ -0,0 +1,1 @@
+var json_Airports_16 = {"type":"FeatureCollection","name":"Airports_16","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Object_typ":"Airport"},"geometry":{"type":"Point","coordinates":[14.37982286282419,67.270418943633203]}}]}
